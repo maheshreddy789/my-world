b45449aa5eb17e754f68cf05a010963b87ed8f4d
maheshreddy789 <95697677+maheshreddy789@users.noreply.github.com>
2021-12-08 20:19:37 +0530

    Update mahesh.h

--- mahesh.h
+++ mahesh.h
@@ -2,3 +2,10 @@ hi mahesh how r u
 hello 
 hi sam ..whre r u now
 sam working on automotive technology in ROLLS ROYCE COMPANY.
+#include<stdio.h>
+  int main()
+{
+int a,b;
+  int c=a+b;
+  printf("%d",a,b);
+}
